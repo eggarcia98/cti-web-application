dc5f57aa4111f779c913bbae32e91ee4c236a4d5
Erick Garcia <eggarcia9814@gmail.com>
2024-05-19 13:39:10 +1000

    catching errors when the api is unavailable

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -4,17 +4,20 @@ import { PUBLIC_BACKEND_HOST, PUBLIC_BACKEND_PORT } from '$env/static/public';
 export const load: LayoutServerLoad = async ({ fetch, cookies, route }) => {
 	const AuthorizationToken = cookies.get('AuthorizationToken');
 
-	const response = await fetch(
-		`${PUBLIC_BACKEND_HOST}:${PUBLIC_BACKEND_PORT}/api/user/authorizate`,
-		{
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({ AuthorizationToken })
-		}
-	);
-
-	const data = await response.json();
-	return data;
+	try {
+		const response = await fetch(
+			`${PUBLIC_BACKEND_HOST}:${PUBLIC_BACKEND_PORT}/api/user/authorizate`,
+			{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({ AuthorizationToken })
+			}
+		);
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		return {};
+	}
 };
--- src/routes/products/+page.server.ts
+++ src/routes/products/+page.server.ts
@@ -2,8 +2,12 @@ import type { PageServerLoad } from './$types';
 import { PUBLIC_BACKEND_HOST, PUBLIC_BACKEND_PORT } from '$env/static/public';
 
 export const load: PageServerLoad = async ({ fetch }) => {
-	const res = await fetch(`${PUBLIC_BACKEND_HOST}:${PUBLIC_BACKEND_PORT}/api/products`);
-	const data = await res.json();
+	try {
+		const res = await fetch(`${PUBLIC_BACKEND_HOST}:${PUBLIC_BACKEND_PORT}/api/products`);
+		const data = await res.json();
 
-	return { products: data };
+		return { products: data };
+	} catch (error) {
+		return { products: [] };
+	}
 };
